f66d7ff15143cf1c9b76cf450559cde5ca79d7e0
JoseJoenk <jmjoenk@gmail.com>
2023-03-18 20:41:36 -0300

    A Base
    começando a base do gerador de tesouros T20

---
+++ tesouro-T20/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+function gitem(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+function gmoney(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
